26b9be250d8dc23c76ad6c7b2dd85d0ed16a1551
Luka <lukametr@gmail.com>
2025-06-16 05:16:02 +0400

    fix: remove unused imports from main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
